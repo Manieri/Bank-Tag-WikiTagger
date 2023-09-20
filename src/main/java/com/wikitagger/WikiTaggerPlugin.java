91a354da9bd4964b40e9c47d547f818875eae2a4
Michael Manieri <github@michaelmanieri.com>
2023-09-20 02:19:12 -0400

    Basics for the Template Repository.

--- src/main/java/com/wikitagger/WikiTaggerPlugin.java
+++ src/main/java/com/wikitagger/WikiTaggerPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.wikitagger;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Bank Tag WikiTagger"
 )
-public class ExamplePlugin extends Plugin
+public class WikiTaggerPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private WikiTaggerConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	WikiTaggerConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(WikiTaggerConfig.class);
 	}
 }
--- src/test/java/com/wikitagger/WikiTaggerPluginTest.java
+++ src/test/java/com/wikitagger/WikiTaggerPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.wikitagger;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class WikiTaggerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(WikiTaggerPlugin.class);
 		RuneLite.main(args);
 	}
 }
